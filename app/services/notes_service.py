e1834f781b7d20ba44f51ad1d05ca93fd47f7b66
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-09-16 19:37:00 +0545

    hi

--- app/services/notes_service.py
+++ app/services/notes_service.py
@@ -1,52 +1,48 @@
 from app.models.note import Note
-from datetime import datetime, timedelta, timezone
-from app.config import settings
-from sqlalchemy.exc import IntegrityError
-
+from uuid import UUID, uuid4
 from sqlmodel.ext.asyncio.session import AsyncSession
-from app.schemas.note import NoteCreate,NoteRead,NoteUpdate
-
+from app.schemas.note import NoteCreate,NoteUpdate
+from sqlmodel import select 
 
 async def create_note(session:AsyncSession,note_in:NoteCreate)->Note:
     db_note=Note(
         title=note_in.title,
         content=note_in.content
     )
-    try:
-        session.add(db_note)
-        await session.commit()
-        await session.refresh(db_note)
-        return db_note 
-    except IntegrityError as e:
-        session.rollback()
-        raise ValueError("Database error while creating product") from e
+    session.add(db_note)
+    await session.commit()
+    await session.refresh(db_note)
+    return db_note 
+   
 
     
 async def get_notes(session:AsyncSession,skip:int=0,limit:int=100):
-    notes=session.query(Note).offset(skip).limit(limit).all()
-    return notes
+    result=await session.exec(select(Note).offset(skip).limit(limit))
+    return list(result.all())
 
 
-async def get_note_by_id(session:AsyncSession,id:int)->Note|None:
-    note=session.query(Note).filter(Note.id==id).first()
+async def get_note_by_id(session:AsyncSession,id:UUID)->Note|None:
+    result=await session.exec(select(Note).where(Note.id==id))
+    note=result.first()
     return note
 
     
-async def update_note(session:AsyncSession,id:int,updates:NoteUpdate):
-    note=get_note_by_id(session,id)
+async def update_note(session:AsyncSession,id:UUID,updates:NoteUpdate):
+    note=await get_note_by_id(session,id)
     if not Note:
         raise ValueError("No note found")
+    data=updates.model_dump(exclude_unset=True)
+    note.sqlmodel_update(data)
+    await session.commit()
+    await session.refresh(note)
+    return note
 
 
 
-async def delete_note(session:AsyncSession,id:int):
-    note=get_note_by_id(session,id)
+async def delete_note(session:AsyncSession,id:UUID)->None:
+    note=await get_note_by_id(session,id)
     if not note:
         raise ValueError("no note found")
-    try:
-        session.delete(note)
-        session.commit()
-        return True
-    except IntegrityError as e:
-        session.rollback()
-        raise ValueError("Cannot delete product due to foreign key constraints") from e
+    await session.delete(note)
+    await session.commit()
+    return True
