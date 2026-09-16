207c3dbdb75fb6052f0661131025d4bb70b42f8a
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-09-16 19:06:35 +0545

    services

---
+++ app/services/notes_service.py
@@ -0,0 +1,14 @@
+from app.models.note import Note
+from datetime import datetime, timedelta, timezone
+from app.config import settings
+
+async def create_note():
+    pass
+async def get_notes():
+    pass
+async def get_note_by_id():
+    pass
+async def update_note():
+    pass
+async def delete_note():
+    pass
